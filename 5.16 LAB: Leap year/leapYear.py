79ff5cb5a151a4bf0cc27a665d01919d41c24e66
akrama23 <arundelfootball23@gmail.com>
2025-01-30 20:25:35 -0500

    leapYear

---
+++ 5.16 LAB: Leap year/leapYear.py
@@ -0,0 +1,9 @@
+
+is_leap_year = False
+   
+year = int(input())
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(year,'- leap year')
+else:
+    print(year,'- not a leap year')
